c461886e0c2037f88737c8a634029486f30fa3ff
fly2tomato <xuju5220@gmail.com>
2015-06-02 16:22:29 +0200

    3

--- app/src/main/java/com/starwanmeigo/xu/gps_wifi_combine/MainActivity.java
+++ app/src/main/java/com/starwanmeigo/xu/gps_wifi_combine/MainActivity.java
@@ -23,16 +23,25 @@ import java.util.List;
 public class MainActivity extends ActionBarActivity {
     // 定义LocationManager对象
     private final String LOCATION_VALUE = "locationValue";
+    private int gps_size =25;
     private LocationManager locationManager;
     //private MainActivity mActivity;
     private Location mCurrentLocation;
     private double longitude_gps = 0;
     private double latitude_gps = 0;
+    private double longitude_gps_sum = 0;
+    private double latitude_gps_sum = 0;
+    private double accuracy_gps_sum = 0;
+    private double longitude_gps_average = 0;
+    private double latitude_gps_average = 0;
+    private double accuracy_gps_average = 0;
     private double speed_gps = 0;
     private double bearing_gps = 0;
     private double accuracy_gps = 0;
-    private double [] longitude_gps_array;
-    private double [] latitude_gps_array;
+    private double [] longitude_gps_array = new double[gps_size];
+    private double [] latitude_gps_array = new double[gps_size];
+    private double [] accuracy_gps_array = new double[gps_size];
+    private double [] gpsSignals = new double[gps_size];
 
     //set coordinates of routers
     double ap1_x = 0;
@@ -42,11 +51,17 @@ public class MainActivity extends ActionBarActivity {
     double ap3_x = 5.65;
     double ap3_y = 1.30;
     //collect 20 data
-    int arraySize = 50;
+    private int arraySize = 50;
+    private int countNumber = 5;
     private double x_wifi = 0;
     private double y_wifi = 0;
-    private double [] x_wifi_array;
-    private double [] y_wifi_array;
+    private double [] x_wifi_array = new double[arraySize];
+    private double [] y_wifi_array = new double[arraySize];
+    private double x_wifi_array_sum = 0;
+    private double y_wifi_array_sum = 0;
+    private double x_wifi_array_average = 0;
+    private double y_wifi_array_average = 0;
+
 
     private double accuracy_wifi = 4;
 
@@ -58,48 +73,69 @@ public class MainActivity extends ActionBarActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final LinearLayout allAP = (LinearLayout) findViewById(R.id.allAP);
-        final ProgressBar loadingSpinner = (ProgressBar) findViewById(R.id.loadingSpinner);
+        final LinearLayout gpsAP = (LinearLayout) findViewById(R.id.gpsAP);
+        final LinearLayout wifiAP = (LinearLayout) findViewById(R.id.wifiAP);
+        final LinearLayout kfAP = (LinearLayout) findViewById(R.id.kfAP);
+        final ProgressBar loadingSpinnerForGPS = (ProgressBar) findViewById(R.id.loadingSpinnerForGPS);
+        final ProgressBar loadingSpinnerForWifi = (ProgressBar) findViewById(R.id.loadingSpinnerForWifi);
         final TextView Longitude = (TextView) findViewById(R.id.gps_longitude);
         final TextView Latitude = (TextView) findViewById(R.id.gps_latitude);
         final TextView Accuracy = (TextView) findViewById(R.id.gps_accuracy);
-        final TextView X_WIFI = (TextView) findViewById(R.id.wifi_x);
-        final TextView Y_WIFI = (TextView) findViewById(R.id.wifi_y);
+        final TextView X1_WIFI = (TextView) findViewById(R.id.wifi_x1);
+        final TextView Y1_WIFI = (TextView) findViewById(R.id.wifi_y1);
+        final TextView X2_WIFI = (TextView) findViewById(R.id.wifi_x2);
+        final TextView Y2_WIFI = (TextView) findViewById(R.id.wifi_y2);
+        final TextView X3_WIFI = (TextView) findViewById(R.id.wifi_x3);
+        final TextView Y3_WIFI = (TextView) findViewById(R.id.wifi_y3);
+        final TextView X4_WIFI = (TextView) findViewById(R.id.wifi_x4);
+        final TextView Y4_WIFI = (TextView) findViewById(R.id.wifi_y4);
+        final TextView X5_WIFI = (TextView) findViewById(R.id.wifi_x5);
+        final TextView Y5_WIFI = (TextView) findViewById(R.id.wifi_y5);
         final TextView Longitude_KF = (TextView) findViewById(R.id.kf_longitude);
         final TextView Latitude_KF = (TextView) findViewById(R.id.kf_latitude);
         Button startBtn = (Button) findViewById(R.id.startbtn);
+        Button calcBtn = (Button) findViewById(R.id.calcbtn);
+
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //make up a new thread to get gpsInformation
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        //update UI
-                        allAP.post(new Runnable() {
+                        gpsAP.post(new Runnable() {
                             @Override
                             public void run() {
-                                allAP.setVisibility(View.GONE);
-                                loadingSpinner.setVisibility(View.VISIBLE);
+                                gpsAP.setVisibility(View.GONE);
+                                loadingSpinnerForGPS.setVisibility(View.VISIBLE);
                             }
                         });
+                        for(int i =0;i< gps_size;i++){
+                            gpsSignals  = getGpsInfo();
+                            longitude_gps_array [i] = gpsSignals[0];
+                            latitude_gps_array [i] = gpsSignals[1];
+                            accuracy_gps_array [i] = gpsSignals [2];
+                            longitude_gps_sum += longitude_gps_array[i];
+                            latitude_gps_sum += latitude_gps_array[i];
+                            accuracy_gps_sum += accuracy_gps_array[i];
 
-
-
-
-
-                        //update UI
-                        allAP.post(new Runnable() {
+                            try{
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        longitude_gps_average = longitude_gps_sum/gps_size;
+                        latitude_gps_average = latitude_gps_sum/gps_size;
+                        accuracy_gps_average = accuracy_gps_sum/gps_size;
+                        gpsAP.post(new Runnable() {
                             @Override
                             public void run() {
-                                allAP.setVisibility(View.VISIBLE);
-                                loadingSpinner.setVisibility(View.GONE
-                                );
-                                Longitude.setText(Double.toString(longitude_gps));
-                                Latitude.setText(Double.toString(latitude_gps));
-                                Accuracy.setText(Double.toString(accuracy_gps));
-
+                                gpsAP.setVisibility(View.VISIBLE);
+                                loadingSpinnerForGPS.setVisibility(View.GONE);
+                                Longitude.setText(Double.toString(longitude_gps_average));
+                                Latitude.setText(Double.toString(latitude_gps_average));
+                                Accuracy.setText(Double.toString(accuracy_gps_average));
                             }
                         });
                     }
@@ -109,7 +145,14 @@ public class MainActivity extends ActionBarActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        for(int count=0;count<5;count++){
+                        wifiAP.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                wifiAP.setVisibility(View.GONE);
+                                loadingSpinnerForWifi.setVisibility(View.VISIBLE);
+                            }
+                        });
+                        for(int count=0;count<countNumber;count++){
                             double rssiLevel_2_sum = 0;
                             double[] routerInfoArray1 = new double[arraySize];
                             double[] routerInfoArray2 = new double[arraySize];
@@ -166,93 +209,78 @@ public class MainActivity extends ActionBarActivity {
                             final double YFloat = calclocation.getLocationY();
                             x_wifi_array [count] = XFloat;
                             y_wifi_array [count] = YFloat;
-                            allAP.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    X_WIFI.setText(Double.toString(XFloat));
-                                    Y_WIFI.setText(Double.toString(YFloat));
-                                }
-                            });
                         }
-                    }
-                }).start();
-                
-                //make up a new thread to calculate the Longitude and Latitude with Kalman Filter
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        //we should convert Cartesian To Lon and Lat here!
-
-                        //
-                        kalmanFilter KF = new kalmanFilter(longitude_gps,latitude_gps, x_wifi_array,y_wifi_array ,accuracy_gps,accuracy_wifi);
-                        double X = KF.kalman_Filter_Process_X();
-                        double Y = KF.kalman_Filter_Process_Y();
-                        allAP.post(new Runnable() {
+                        wifiAP.post(new Runnable() {
                             @Override
                             public void run() {
-                                Longitude_KF.setText(Double.toString(longitude_kf));
-                                Latitude_KF.setText(Double.toString(latitude_kf));
+                                wifiAP.setVisibility(View.VISIBLE);
+                                loadingSpinnerForWifi.setVisibility(View.GONE);
+                                X1_WIFI.setText(Double.toString(x_wifi_array[0]));
+                                X2_WIFI.setText(Double.toString(x_wifi_array[1]));
+                                X3_WIFI.setText(Double.toString(x_wifi_array[2]));
+                                X4_WIFI.setText(Double.toString(x_wifi_array[3]));
+                                X5_WIFI.setText(Double.toString(x_wifi_array[4]));
+                                Y1_WIFI.setText(Double.toString(y_wifi_array[0]));
+                                Y2_WIFI.setText(Double.toString(y_wifi_array[1]));
+                                Y3_WIFI.setText(Double.toString(y_wifi_array[2]));
+                                Y4_WIFI.setText(Double.toString(y_wifi_array[3]));
+                                Y5_WIFI.setText(Double.toString(y_wifi_array[4]));
                             }
                         });
                     }
                 }).start();
             }
         });
+        calcBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //calculate the Longitude and Latitude with Kalman Filter
+                //we should convert Cartesian To Lon and Lat here!
+
+                //
+                longitude_gps = longitude_gps_average;
+                latitude_gps = latitude_gps_average;
+                accuracy_gps = accuracy_gps_average;
+                y_wifi_array = new double[]{51, 53, 54, 50, 52};
+                x_wifi_array = new double[]{11, 14 ,15 ,13, 16};
+                kalmanFilter KF = new kalmanFilter(longitude_gps,latitude_gps, x_wifi_array,y_wifi_array ,accuracy_gps,accuracy_wifi);
+                final double X = KF.kalman_Filter_Process_X();
+                final double Y = KF.kalman_Filter_Process_Y();
+                Longitude_KF.setText(Double.toString(X));
+                Latitude_KF.setText(Double.toString(Y));
+
+
+            }
+        });
+
     }
 
-    protected double [] getGpsInfo(){
-        double longitude_gps_sum = 0;
-        double latitude_gps_sum = 0;
-        for(int i =0;i<arraySize;i++){
-            // 获取系统LocationManager服务
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            // 从GPS获取最近的定位信息
-            mCurrentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-
-            if(mCurrentLocation != null){
-                longitude_gps = mCurrentLocation.getLongitude();
-                latitude_gps = mCurrentLocation.getLatitude();
-                speed_gps = mCurrentLocation.getSpeed();
-                bearing_gps = mCurrentLocation.getBearing();
-                accuracy_gps = mCurrentLocation.getAccuracy();
-                Log.d("Longitude","Longitude:"+longitude_gps);
-                Log.d("Latitude","Latitude:"+latitude_gps);
-                Log.d("speed","speed:"+speed_gps);
-                Log.d("bearing","bearing:"+bearing_gps);
-                Log.d("accuracy","accuracy:"+accuracy_gps);
-            }else {
-                longitude_gps = Double.parseDouble(null);
-                latitude_gps = Double.parseDouble(null);
-            }
-
-            longitude_gps_array [i] = longitude_gps;
-            latitude_gps_array [i] = latitude_gps;
-
-            try{
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    private double [] getGpsInfo() {
+        // 获取系统LocationManager服务
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // 从GPS获取最近的定位信息
+        mCurrentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(mCurrentLocation != null){
+            longitude_gps = mCurrentLocation.getLongitude();
+            latitude_gps = mCurrentLocation.getLatitude();
+            speed_gps = mCurrentLocation.getSpeed();
+            bearing_gps = mCurrentLocation.getBearing();
+            accuracy_gps = mCurrentLocation.getAccuracy();
+            Log.d("Longitude","Longitude:"+longitude_gps);
+            Log.d("Latitude","Latitude:"+latitude_gps);
+            Log.d("speed","speed:"+speed_gps);
+            Log.d("bearing","bearing:"+bearing_gps);
+            Log.d("accuracy","accuracy:"+accuracy_gps);
+        }else {
+            longitude_gps = Double.parseDouble(null);
+            latitude_gps = Double.parseDouble(null);
+            accuracy_gps = Double.parseDouble(null);
         }
-
-        for(int i=0;i<arraySize;i++){
-            longitude_gps_sum = longitude_gps_sum + longitude_gps_array [i];
-            latitude_gps_sum = latitude_gps_sum + latitude_gps_array [i];
-        }
-
-        longitude_gps = longitude_gps_sum/arraySize;
-        latitude_gps = latitude_gps_sum/arraySize;
-
-        double [] lon_lat = new double[2];
-        lon_lat [0] = longitude_gps;
-        lon_lat [1] = latitude_gps;
-
-        return lon_lat;
+        return new double[]{longitude_gps, latitude_gps, accuracy_gps};
     }
 
-    protected double [] getRouterInfo(){
+
+    private double [] getRouterInfo(){
         List<ScanResult> wifiList;
         WifiManager wifiManager;
         wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
@@ -280,7 +308,7 @@ public class MainActivity extends ActionBarActivity {
                 level_3 = level[i];
             }
             else {
-                //Log.d("MainActivity", "finding the APs!" );
+                Log.d("MainActivity", "finding the APs!" );
                 //Toast.makeText(this, "finding the APs!", Toast.LENGTH_SHORT).show();
             }
         }
